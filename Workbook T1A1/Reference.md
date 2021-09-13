6430d1b31d8e2ced104e1aa9158ef323b745ef3f
tfxl <CAB022105@coderacademy.edu.au>
2021-09-13 18:26:50 +1000

    initial commit for workbook T1A1 including solid amount of workbook completed and some code and notes as well

---
+++ Workbook T1A1/Reference.md
@@ -0,0 +1,32 @@
+Author Surname, Initial(s). (Year) Title. Edition (if not first edition). Place of publication: Publisher.
+
+Bryman, A. (2016) Social research methods. 5th edn. Oxford: Oxford University Press.
+
+
+
+
+
+
+Sebesta,R. (2013) Programming the World Wide Web, 7th ed. Boston, Person Education, Inc, pp4
+
+
+
+![GitHub Logo](/images/logo.png)
+Format: ![Alt Text](url)
+
+
+http://github.com - automatic!
+[GitHub](http://github.com)
+
+Indented Lines
+> We're living the future so
+> the present is our past.
+
+
+inline code
+
+I think you should use an
+`<addr>` element here instead.
+
+
+https://guides.github.com/features/mastering-markdown/
---
+++ Workbook T1A1/prime_numbers.rb
@@ -0,0 +1,30 @@
+start_time = Time.new
+
+prime_numbers = [2]
+# as this problem requests values 0-100, we can start our array of prime numbers with 2, the first prime number
+# if the range was more diverse, a conditional statement would be appropriate
+
+investigated_number = 3
+# as per above, given the range parameters, our investigation starts at number 3, as 2 already recorded in array
+
+possible_factor = 2
+# the first possible factor to investigate is 2 (even numbers)
+
+while investigated_number < 1001
+
+  if investigated_number % possible_factor == 0
+    possible_factor = 2
+    investigated_number += 1
+    next
+  elsif possible_factor > (investigated_number / possible_factor)
+    prime_numbers.append(investigated_number)
+    possible_factor = 2
+    investigated_number += 1
+    next
+  else possible_factor += 1
+  end
+end
+
+p prime_numbers
+
+puts '-' * 35, "Time to complete process #{Time.now - start_time}", '-' * 35
